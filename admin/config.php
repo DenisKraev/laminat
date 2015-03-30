5476a266871042b147012b3f186f5e9e0e83a70b
DenisKraev <krvdns@mail.ru>
2015-03-30 12:53:36 +0400

    Установленная дефолтная система

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://laminat/admin/');
+define('HTTP_CATALOG', 'http://laminat/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://laminat/admin/');
+define('HTTPS_CATALOG', 'http://laminat/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\OpenServer\domains\laminat/admin/');
+define('DIR_SYSTEM', 'D:\OpenServer\domains\laminat/system/');
+define('DIR_DATABASE', 'D:\OpenServer\domains\laminat/system/database/');
+define('DIR_LANGUAGE', 'D:\OpenServer\domains\laminat/admin/language/');
+define('DIR_TEMPLATE', 'D:\OpenServer\domains\laminat/admin/view/template/');
+define('DIR_CONFIG', 'D:\OpenServer\domains\laminat/system/config/');
+define('DIR_IMAGE', 'D:\OpenServer\domains\laminat/image/');
+define('DIR_CACHE', 'D:\OpenServer\domains\laminat/system/cache/');
+define('DIR_DOWNLOAD', 'D:\OpenServer\domains\laminat/download/');
+define('DIR_LOGS', 'D:\OpenServer\domains\laminat/system/logs/');
+define('DIR_CATALOG', 'D:\OpenServer\domains\laminat/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', '123654');
+define('DB_DATABASE', 'laminat_dev');
+define('DB_PREFIX', '');
+?>
